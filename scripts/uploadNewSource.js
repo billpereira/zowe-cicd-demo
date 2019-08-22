72a14b664d5387b64cdac5d9914bf95de2f2c429
William Pereira <william.pereira@me.com>
2019-08-22 16:48:52 -0300

    added sample members

--- scripts/uploadNewSource.js
+++ scripts/uploadNewSource.js
@@ -1,6 +1,7 @@
 const { execSync } = require('child_process');
 
 console.log(execSync(`zowe jobs submit lf "scripts/iebcopy.jcl" -d output`).toString())
-console.log(execSync(`zowe files ul ftds "app/job01.jcl" "A028356.CICD.LIB(JOB01JCL)"`).toString())
-console.log(execSync(`zowe files ul ftds "app/job02.jcl" "A028356.CICD.LIB(JOB02JCL)"`).toString())
-console.log(execSync(`zowe files ul ftds "app/hello.cbl" "A028356.CICD.HELLO.CBL"`).toString())
+console.log(execSync(`zowe files ul ftds "app/job01.jcl" "LBI9.CICD.LIB(JOB01JCL)"`).toString())
+console.log(execSync(`zowe files ul ftds "app/job02.jcl" "LBI9.CICD.LIB(JOB02JCL)"`).toString())
+console.log(execSync(`zowe files ul ftds "app/hello.cbl" "LBI9.CICD.HELLO.CBL"`).toString())
+console.log(execSync(`zowe files ul ftds "app/input.txt" "LBI9.CICD.INPUT"`).toString())
