bd04f1fbd71a808543db9ac8a49fba3357d0e4ed
William Pereira <william.pereira@me.com>
2019-08-22 16:24:30 -0300

    added sample members

--- scripts/callServiceNow.sh
+++ scripts/callServiceNow.sh
@@ -1,6 +1,8 @@
-curl “https://ibmpb3.service-now.com/api/x_ibmg5_devops_cic/v1/cdci” \
+#!/bin/bash
+curl “http://ibmpb3.service-now.com/api/x_ibmg5_devops_cic/v1/cdci” \
 --request POST \
 --header “Accept:application/json” \
 --header “Content-Type:application/json” \
 --data “{\“assignment_group\“:\“Software\“,\“assigned_to\“:\“Thiago Gomes Muntoreanu\“,\“crq_status\“:\“\”,\“request_status\“:\“\”,\“request_summary\“:\“CDCI new JCL and Cobol\“}” \
---user ‘admin’:‘admin’
+--user ‘admin’:‘admin’ \
+-k
