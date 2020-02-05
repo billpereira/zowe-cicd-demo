bfc622e2b899cc0717433f06e424780770d70f84
billpereira <william.pereira@me.com>
2020-02-04 21:29:36 -0300

    Update createTestJobs.js

--- scripts/createTestJobs.js
+++ scripts/createTestJobs.js
@@ -4,9 +4,7 @@ const path = require('path');
 const createTestCopy = (jclName, jclDirectory) => {
 	fs.readFile(path.join(jclDirectory, jclName), (err, data) => {
 		if (err) throw err;
-
 		let newJcl = data.toString().replace(',SYSAFF=*', ',SYSAFF=*,TYPRUN=SCAN');
-
 		fs.writeFile(`test.${jclName}`, newJcl, err => {
 			if (err) throw err;
 			console.log(`The file test.${jclName} has been saved!`);
@@ -20,11 +18,8 @@ const checkJcl = () => {
 		if (err) {
 			return console.log('Unable to scan directory: ' + err);
 		}
-
 		files = files.filter(element => element.indexOf('jcl') > 0);
-
 		console.log(files);
-
 		files.forEach(jcl => createTestCopy(jcl, jclDirectory));
 	});
 };
